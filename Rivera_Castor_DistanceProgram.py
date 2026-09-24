bf63fea5dcaeadb79033cadeff0521658bcc020c
castorrivera <ccrivera@carc.pshs.edu.ph>
2026-09-24 08:38:36 +0800

    Refactor comments and maintain distance calculation
    Updated comments for clarity and retained original functionality.

--- Rivera_Castor_DistanceProgram.py
+++ Rivera_Castor_DistanceProgram.py
@@ -1,5 +1,7 @@
+#Imported the math module so we can use math formulas in python
 import math
 
+#Input the coordinates so that the distance can be calculated
 x1 = float(input("enter number for x¹ "))
 print("number entered")
 y1 = float(input("enter number for y¹ "))
@@ -11,10 +13,11 @@ print("number entered")
 
 x3 = x2 - x1
 y3 = y2 - y1
-
+#giving the answers to the power of 2
 answer1 = math.pow(x3, 2)
 answer2 = math.pow(y3, 2)
 
+#Adding both answers together
 subfinal = answer1 + answer2
 
 final = math.sqrt(subfinal)
